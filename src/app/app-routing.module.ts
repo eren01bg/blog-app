21524e20430541302f25b3fd8462da91d0afacdb
eren01bg <eren01gs@outlook.com>
2023-08-11 23:36:49 +0300

    Project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PostComponent } from './components/post/post.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'login', component : LoginComponent},
+  {path: 'register', component : RegisterComponent},
+  {path: 'post', component : PostComponent},
+  {path: 'category', component : CategoryComponent},
+  {path: 'create-blog', component : CreateBlogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PostComponent } from './components/post/post.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PostComponent,
+    CategoryComponent,
+    CreateBlogComponent,
   ],
   imports: [
     BrowserModule,
